80a0c87bc942a6f742ae96a1816d681fd3fc6f5e
taro.kirameki <tkirameki3@gmail.com>
2022-08-23 07:01:27 +0000

    CRUD tasks

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,17 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'TasksController@index');
+
+Route::resource('tasks', 'TasksController');
+
+
+//Route::get('tasks/{id}', 'TasksController@show');
+//Route::post('tasks', 'TasksController@store');
+//Route::put('tasks/{id}', 'TasksController@update');
+//Route::delete('tasks/{id}', 'TasksController@destroy');
+
+//Route::get('tasks', 'TasksController@index')->name('tasks.index');
+//Route::get('tasks/create', 'TasksController@create')->name('tasks.create');
+//Route::get('tasks/{id}/edit', 'TasksController@edit')->name('tasks.edit');
+
